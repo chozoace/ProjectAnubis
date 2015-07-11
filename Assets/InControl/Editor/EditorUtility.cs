79b289b43694ca846254b82719c7fa51085c27c5
Roger Rosa <chozoace@gmail.com>
2015-07-11 12:55:05 -0400

    First Commit

---
+++ Assets/InControl/Editor/EditorUtility.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace InControl
+{
+	public static class EditorUtility
+	{
+		public static T LoadAssetAtPath<T>( string assetPath )
+			where T: UnityEngine.Object
+		{
+			#if UNITY_5
+			return AssetDatabase.LoadAssetAtPath<T>( assetPath );
+			#else
+			return Resources.LoadAssetAtPath<T>( assetPath );
+			#endif
+		}
+	}
+}
+#endif
---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class PlayerControllerScript : MonoBehaviour 
+{
+    [SerializeField] float _maxSpeed = 30;
+    [SerializeField] float _jumpSpeed = 3;
+    float _currentXSpeed;
+    
+	void Start () 
+    {
+	    //Testing
+	}
+	
+	void Update () 
+    {
+        var inputDevice = InputManager.ActiveDevice;
+
+        //X Movement
+        _currentXSpeed = (_maxSpeed * inputDevice.LeftStickX.Value);
+        Vector2 v = gameObject.GetComponent<Rigidbody2D>().velocity;
+        v.x = _currentXSpeed;
+        gameObject.GetComponent<Rigidbody2D>().velocity = v;
+
+        if(inputDevice.Action1.WasPressed)
+        {
+            Jump();
+        }
+	}
+
+    void Jump()
+    {
+        Vector2 v = gameObject.GetComponent<Rigidbody2D>().velocity;
+        v.y = _jumpSpeed;
+        gameObject.GetComponent<Rigidbody2D>().velocity = v;
+    }
+}
