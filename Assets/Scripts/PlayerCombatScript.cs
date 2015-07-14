cb59e9b9be3c50ee1ba8a892e614573acfb3b293
Roger Rosa <chozoace@gmail.com>
2015-07-13 23:45:33 -0400

    Added ground check and started adding attacks

---
+++ Assets/Scripts/PlayerCombatScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerCombatScript : MonoBehaviour 
+{
+    [SerializeField] List<GameObject> _openingAttacks;
+
+	void Start () 
+    {
+	    
+	}
+
+    public void DoMove()
+    {
+        //Will this instantiate the prefab? take in a parameter?
+    }
+	
+    void FixedUpdate()
+    {
+
+    }
+
+	void Update () 
+    {
+	    
+	}
+}
--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -6,13 +6,24 @@ public class PlayerControllerScript : MonoBehaviour
 {
     [SerializeField] float _maxSpeed = 30;
     [SerializeField] float _jumpSpeed = 3;
+    [SerializeField] Transform _groundCheck;
+    float _groundRadius = .32f;
+    [SerializeField] LayerMask _whatIsGround;
     float _currentXSpeed;
+
+    bool _grounded = false;
+    bool _attacking = false;
     
 	void Start () 
     {
 	    //Testing
 	}
-	
+
+    void FixedUpdate()
+    {
+        _grounded = Physics2D.OverlapCircle(_groundCheck.position, _groundRadius, _whatIsGround);
+    }
+
 	void Update () 
     {
         var inputDevice = InputManager.ActiveDevice;
@@ -22,11 +33,17 @@ public class PlayerControllerScript : MonoBehaviour
         Vector2 v = gameObject.GetComponent<Rigidbody2D>().velocity;
         v.x = _currentXSpeed;
         gameObject.GetComponent<Rigidbody2D>().velocity = v;
-
-        if(inputDevice.Action1.WasPressed)
+        
+        if(inputDevice.Action1.WasPressed && _grounded)
         {
             Jump();
         }
+        else if(inputDevice.Action3.WasPressed && _grounded)
+        {
+            //What will do move do? 
+            gameObject.GetComponent<PlayerCombatScript>().DoMove();
+        }
+
 	}
 
     void Jump()
